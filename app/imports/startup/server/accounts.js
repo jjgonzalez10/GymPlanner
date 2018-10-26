6ff58f6e43216b26e79ad4402b4c84e5175d1e6b
Nicolas <n.simmonds10@uniandes.edu.co>
2018-10-25 20:54:48 -0500

    base

---
+++ app/imports/startup/server/accounts.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { Roles } from 'meteor/alanning:roles';
+
+/* eslint-disable no-console */
+
+function createUser(email, password, role) {
+  console.log(`  Creating user ${email}.`);
+  const userID = Accounts.createUser({
+    username: email,
+    email: email,
+    password: password,
+  });
+  if (role === 'admin') {
+    Roles.addUsersToRoles(userID, 'admin');
+  }
+}
---
+++ app/imports/ui/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Image} from 'semantic-ui-react';
+
+/** A simple static component to render some text for the landing page. */
+class Landing extends React.Component {
+  render() {
+    return (
+      <Image id="main" src="/images/main.jpg"/>
+
+      );
+  }
+}
+
+export default Landing;
